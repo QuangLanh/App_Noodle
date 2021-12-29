5a9646ed60c7c8b568dd89681c10a00f41cf8b7a
QuangLanh <tpsf.q.lanh@gmail.com>
2021-12-29 11:01:16 +0700

    add firebase

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,13 +2,11 @@ import * as firebase from 'firebase';
 //import 'firebase/database';
 
 const Config = {
-  apiKey: 'AIzaSyDn79ys3LZrEPujoFJPkh57gZ-x-DD8lTM',
-  authDomain: 'awesome-34dc7.firebaseapp.com',
-  databaseURL: 'https://awesome-34dc7-default-rtdb.firebaseio.com',
-  projectId: 'awesome-34dc7',
-  storageBucket: 'awesome-34dc7.appspot.com',
-  messagingSenderId: '896277201741',
-  appId: '1:896277201741:web:6345b8b5a7eb9809632d04',
-  measurementId: 'G-01G0FQNG3K',
+  apiKey: "AIzaSyC42Rfx14aN25tXfPfahkOxJ84gZpPVWbM",
+  authDomain: "quanglanh2508.firebaseapp.com",
+  projectId: "quanglanh2508",
+  storageBucket: "quanglanh2508.appspot.com",
+  messagingSenderId: "856864339972",
+  appId: "1:856864339972:web:a281286a8582f82d2bdd51"
 };
 export default firebaseConfig = firebase.initializeApp(Config);
