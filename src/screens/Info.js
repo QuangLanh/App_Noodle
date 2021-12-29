8f1a7c4c52ec28ae5ef09cd4b19c4751b73db602
QuangLanh <tpsf.q.lanh@gmail.com>
2021-12-29 09:38:22 +0700

    update firebase + redux

--- src/screens/Info.js
+++ src/screens/Info.js
@@ -1,217 +1,391 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, Image, SafeAreaView, ImageBackground, } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import { Text, View, StyleSheet, TouchableOpacity, Image, SafeAreaView, ImageBackground,} from 'react-native';
+import firebase from '../firebase/firebase';
 
-const Info = ({ navigation }) => {
-return (
+const Info = ({navigation}) => {
+  const Hover = useSelector(state => state.NoodlesReducer.hover);
+  const [selectedHover1, setSelectedHover1] = useState(false);
+  const [selectedHover2, setSelectedHover2] = useState(false);
+  const [selectedHover3, setSelectedHover3] = useState(false);
+  const [comeBack, setComeBack] = useState(false);
 
+  //firebase
+  const [data, setData] = useState([]);
+  const getuser = () => {
+    firebase
+      .database()
+      .ref()
+      .child('information')
+      .on('value', snapshot => {
+        var infor = [];
+        snapshot.forEach(child => {
+          let information = {
+            fullname: child.val().fullname,
+            birthay: child.val().birthay,
+            gender: child.val().gender,
+            department: child.val().department,
+          };
+          infor.push(information);
+        });
+        setData(infor);
+        console.log('data', data);
+      });
+  };
+
+  useEffect(() => {
+    getuser();
+  }, []);
+  const setHover1 = hover =>
+    dispatch({
+      type: 'SET_HOVER1',
+      payload: hover,
+    });
+  const setHover2 = hover =>
+    dispatch({
+      type: 'SET_HOVER2',
+      payload: hover,
+    });
+  const setHover3 = hover =>
+    dispatch({
+      type: 'SET_HOVER3',
+      payload: hover,
+    });
+  const dispatch = useDispatch();
+
+  const handle = () => {
+    // redux
+    if (selectedHover1) {
+      setHover1(false);
+      setSelectedHover1(false);
+    }
+
+    if (selectedHover2) {
+      setHover2(false);
+      setSelectedHover2(false);
+    }
+    if (selectedHover3) {
+      setHover3(false);
+      setSelectedHover3(false);
+    }
+
+    navigation.navigate('Done');
+  };
+
+  return (
+    <>
+      {data.length != 0 ? (
         <SafeAreaView style={styles.container}>
-            <ImageBackground
-                source={require('../../images/background.jpg')}
-                resizeMode="cover"
-                style={styles.container}>
-                <View>
-                    <Image style={styles.logo} source={require('../../images/logo.png')} />
-                    <Text style={styles.infotext}>INFORMATION</Text>
-                    <ImageBackground
-                        source={require('../../images/card.png')}
-                        resizeMode="cover"
-                        style={styles.card}>
-                        <View style={{ flexDirection: 'row' }}>
-                            <Image style={styles.user} source={require('../../images/user.jpg')}
-                            />
-                            <View>
-                                <View
-                                    style={{
-                                        flexDirection: 'row',
-                                        marginTop: 10,
-                                        marginLeft: 40,
-                                    }}>
-                                    <Text style={styles.fullname}>Full Name:</Text>
-                                    <Text style={styles.name}>Alice Mie</Text>
-                                </View>
-                                <View
-                                    style={{
-                                        flexDirection: 'row',
-                                        marginLeft: 40,
-                                        marginTop: 5,
-                                    }}>
-                                    <Text style={styles.fullname}>Birthday:</Text>
-                                    <Text style={styles.birthay}> 12/1/1999</Text>
-                                </View>
-                                <View
-                                    style={{
-                                        flexDirection: 'row',
-                                        marginLeft: 40,
-                                        marginTop: 5,
-                                    }}>
-                                    <Text style={styles.fullname}>Gender: </Text>
-                                    <Text style={styles.gender}>Female</Text>
-                                </View>
-                                <View
-                                    style={{
-                                        flexDirection: 'row',
-                                        marginLeft: 40,
-                                        marginTop: 5,
-                                    }}>
-                                    <Text style={styles.fullname}>Department:</Text>
-                                    <Text style={styles.department}>Design</Text>
-                                </View>
-                            </View>
-                        </View>
-                    </ImageBackground>
-
-                    <View style={styles.css_noodles}>
-                        <View>
-                            <Image style={styles.hover} source={require('../../images/hover.png')} />
-
-                            <Image style={styles.lymy} source={require('../../images/lymy.png')} />
-
-                            
-
-                        </View>
-                        <View>
-                            <Image style={styles.hover} source={require('../../images/hover.png')} />
-
-                            <Image style={styles.lymy} source={require('../../images/lymy.png')} />
-
-                         
-
-                        </View>
-
-                        <View>
-                            <Image style={styles.hover} source={require('../../images/hover.png')} />
-                            <Image style={styles.lymy} source={require('../../images/lymy.png')} />
-
-                           
-                        </View>
-                    </View>
-
-                    <View style={{
-                        flexDirection: 'row',
-                        alignSelf: 'center',
-                        marginTop: 10,
-                    }}>
-                        <Text style={{ color: '#D91313', fontWeight: 'bold', fontSize: 15 }}></Text>
-                        <Text style={{ color: '#9C6666', fontSize: 15 }}>
-                            {' '}
-                           3 cups of noodles left this month
-                        </Text>
-                    </View>
-                </View>
-                <TouchableOpacity onPress={() => navigation.navigate('Done')}>
-                    
-                    <Image style={styles.add} source={require('../../images/add.png')} />
-                </TouchableOpacity>
-                <TouchableOpacity
-              onPress={() => navigation.navigate('ErrorScreen')}>
+          <ImageBackground
+            source={require('../../images/background.jpg')}
+            resizeMode="cover"
+            style={styles.container}>
+            <View>
               <Image
-                style={styles.next}
-                source={require('../../images/next.png')}
+                style={styles.logo}
+                source={require('../../images/logo.png')}
+              />
+              <Text style={styles.textinformation}>INFORMATION</Text>
+              <ImageBackground
+                source={require('../../images/card.png')}
+                resizeMode="cover"
+                style={styles.BGcard}>
+                <View style={{flexDirection: 'row'}}>
+                  <Image
+                    style={styles.user}
+                    source={require('../../images/user.jpg')}
+                  />
+                  <View>
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                        marginTop: 10,
+                        marginLeft: 40,
+                      }}>
+                      <Text style={styles.textfullname}>Full Name:</Text>
+                      <Text style={styles.textname}>{data[0].fullname}</Text>
+                    </View>
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                        marginLeft: 40,
+                        marginTop: 5,
+                      }}>
+                      <Text style={styles.textfullname}>Birthday:</Text>
+                      <Text style={styles.textbirthay}> {data[0].birthay}</Text>
+                    </View>
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                        marginLeft: 40,
+                        marginTop: 5,
+                      }}>
+                      <Text style={styles.textfullname}>Gender: </Text>
+                      <Text style={styles.textgender}>{data[0].gender}</Text>
+                    </View>
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                        marginLeft: 40,
+                        marginTop: 5,
+                      }}>
+                      <Text style={styles.textfullname}>Department:</Text>
+                      <Text style={styles.textdepartment}>
+                        {data[0].department}
+                      </Text>
+                    </View>
+                  </View>
+                </View>
+              </ImageBackground>
+
+              <View style={styles.css_noodles}>
+                <View>
+                  {Hover.hover1 ? (
+                    <TouchableOpacity
+                      onPress={() => {
+                        setSelectedHover1(!selectedHover1);
+                      }}>
+                      {selectedHover1 == true && (
+                        <Image
+                          style={styles.hover}
+                          source={require('../../images/hover.png')}
+                        />
+                      )}
+                      <Image
+                        style={styles.noodles}
+                        source={require('../../images/lymy.png')}
+                      />
+                    </TouchableOpacity>
+                  ) : (
+                    <View>
+                      <Image
+                        style={styles.unavailableNoodles}
+                        source={require('../../images/lyhet.png')}
+                      />
+                      <Text style={styles.css_unavailableText}>
+                        Unavailable
+                      </Text>
+                    </View>
+                  )}
+                </View>
+                <View>
+                  {Hover.hover2 ? (
+                    <TouchableOpacity
+                      onPress={() => {
+                        setSelectedHover2(!selectedHover2);
+                      }}>
+                      {selectedHover2 == true && (
+                        <Image
+                          style={styles.hover}
+                          source={require('../../images/hover.png')}
+                        />
+                      )}
+                      <Image
+                        style={styles.noodles}
+                        source={require('../../images/lymy.png')}
+                      />
+                    </TouchableOpacity>
+                  ) : (
+                    <View>
+                      <Image
+                        style={styles.unavailableNoodles}
+                        source={require('../../images/lyhet.png')}
+                      />
+                      <Text style={styles.css_unavailableText}>
+                        Unavailable
+                      </Text>
+                    </View>
+                  )}
+                </View>
+
+                <View>
+                  {Hover.hover3 ? (
+                    <TouchableOpacity
+                      onPress={() => {
+                        setSelectedHover3(!selectedHover3);
+                      }}>
+                      {selectedHover3 == true && (
+                        <Image
+                          style={styles.hover}
+                          source={require('../../images/hover.png')}
+                        />
+                      )}
+                      <Image
+                        style={styles.noodles}
+                        source={require('../../images/lymy.png')}
+                      />
+                    </TouchableOpacity>
+                  ) : (
+                    <View>
+                      <Image
+                        style={styles.unavailableNoodles}
+                        source={require('../../images/lyhet.png')}
+                      />
+                      <Text style={styles.css_unavailableText}>
+                        Unavailable
+                      </Text>
+                    </View>
+                  )}
+                </View>
+              </View>
+
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignSelf: 'center',
+                  marginTop: 10,
+                }}>
+                <Text
+                  style={{color: '#D91313', fontWeight: 'bold', fontSize: 15}}>
+                  {Hover.remain}
+                </Text>
+                <Text style={{color: '#9C6666', fontSize: 15}}>
+                  {' '}
+                  cups of noodles left this month
+                </Text>
+              </View>
+            </View>
+            <TouchableOpacity onPress={handle}>
+              <Image
+                style={styles.buttonget}
+                source={require('../../images/add.png')}
               />
             </TouchableOpacity>
-            </ImageBackground>
+            <TouchableOpacity
+              onPress={() => {
+                setComeBack(!comeBack);
+              }}>
+              {comeBack == true && (
+                <Image
+                  style={styles.buttoncomeback}
+                  source={require('../../images/nextmonth.png')}
+                />
+              )}
+            </TouchableOpacity>
+          </ImageBackground>
         </SafeAreaView>
-    )
-
-
-
-
+      ) : (
+        <></>
+      )}
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    logo: {
-        width: 120,
-        height: 90,
-        alignSelf: 'center',
-        marginTop: 20,
-    },
-    infotext: {
-        alignSelf: 'center',
-        fontWeight:'900',
-        fontSize: 30,
-        marginTop: 20,
-        fontFamily: 'SVN-Nexa Rust Slab Black Shadow',
-        color: '#C71A1A',
-    },
-    card: {
-        width: 350,
-        height: 130,
-        marginTop: 20,
-        alignSelf: 'center',
-        marginVertical: 10,
-    },
-    user: {
-        width: 70,
-        height: 70,
-        marginTop: 30,
-        marginLeft: 20,
-        borderRadius: 30,
-    },
-    fullname: {
-        marginTop: 3,
-        fontSize: 15,
-        fontWeight: 'bold',
-        color: '#880B0B',
-    },
-    name: {
-        fontSize: 15,
-        color: '#880B0B',
-        marginHorizontal: 30,
-    },
-    lymy: {
-        width: 60,
-        height: 90,
-    },
-    add: {
-        width: 300,
-        height: 50,
-        marginTop: 120,
-        alignSelf: 'center',
-        
-    },
-    css_noodles: {
-        justifyContent: 'space-between',
-        flexDirection: 'row',
-        marginHorizontal: 50,
-        marginTop: 20,
-    },
-   
-    hover: {
-        marginTop: 10,
-        width: 80,
-        height: 80,
-        position: 'absolute',
-        marginLeft: -10,
-    },
-    birthay: {
-        marginLeft: 35,
-        fontSize: 15,
-        color: '#880B0B',
-    },
-    gender: {
-        marginLeft: 45,
-        fontSize: 15,
-        color: '#880B0B',
-    },
-    design: {
-        marginLeft: 40,
-        fontSize: 15,
-        color: '#880B0B',
-    },
-    department: {
-        marginLeft: 15,
-        fontSize: 15,
-        color: '#880B0B',
-    },
-    next: {
-        width: 80,
-        height: 50,
-        alignSelf: 'center',
-        marginLeft: 40,
-        marginTop: 40,
-      },
+  container: {
+    flex: 1,
+  },
+  logo: {
+    width: 120,
+    height: 90,
+    alignSelf: 'center',
+    marginTop: 10,
+  },
+  textinformation: {
+    alignSelf: 'center',
+    fontSize: 30,
+    marginTop: 10,
+    fontFamily: 'SVN-Nexa Rust Slab Black Shadow',
+    color: '#C71A1A',
+  },
+  BGcard: {
+    width: 350,
+    height: 130,
+    alignSelf: 'center',
+    marginVertical: 10,
+  },
+  user: {
+    width: 70,
+    height: 70,
+    marginTop: 30,
+    marginLeft: 20,
+    borderRadius: 30,
+  },
+  textfullname: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#880B0B',
+  },
+  textname: {
+    fontSize: 15,
+    color: '#880B0B',
+    marginHorizontal: 30,
+  },
+  noodles: {
+    width: 60,
+    height: 90,
+  },
+  buttonget: {
+    width: 300,
+    height: 50,
+    marginTop: 60,
+    position: 'absolute',
+    alignSelf: 'center',
+  },
+  buttoncomeback: {
+    width: 300,
+    height: 50,
+  },
+  css_noodles: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginHorizontal: 50,
+    marginTop: 10,
+  },
+  css_hover: {
+    flexDirection: 'row',
+    width: 330,
+    justifyContent: 'space-around',
+    position: 'absolute',
+    alignSelf: 'center',
+  },
+  hover: {
+    marginTop: 10,
+    width: 80,
+    height: 80,
+    position: 'absolute',
+    marginLeft: -10,
+  },
+  css_unavailableNoodles: {
+    flexDirection: 'row',
+    width: 400,
+    justifyContent: 'space-around',
+    position: 'absolute',
+    alignSelf: 'center',
+  },
+  unavailableNoodles: {
+    width: 60,
+    height: 100,
+  },
+  unavailableText: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  css_unavailableText: {
+    fontFamily: 'PaytoneOne-Regular',
+    fontSize: 15,
+    width: 100,
+    color: '#C4C4C4',
+  },
+  textbirthay: {
+    marginLeft: 35,
+    fontSize: 15,
+    color: '#880B0B',
+  },
+  textgender: {
+    marginLeft: 45,
+    fontSize: 15,
+    color: '#880B0B',
+  },
+  textdesign: {
+    marginLeft: 40,
+    fontSize: 15,
+    color: '#880B0B',
+  },
+  textdepartment: {
+    marginLeft: 15,
+    fontSize: 15,
+    color: '#880B0B',
+  },
 });
 export default Info;
