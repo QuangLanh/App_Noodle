65b65865786f315c88beb44b93cab9a181571cc2
QuangLanh <tpsf.q.lanh@gmail.com>
2021-12-29 11:27:47 +0700

    Update Outof.js

--- src/screens/Outof.js
+++ src/screens/Outof.js
@@ -30,44 +30,73 @@ const Outof = ({ navigation }) => {
             {' '}
             machine. Please fill in to continue.
           </Text>
-          <Image
-            style={styles.image_noodles}
-            source={require('../../images/lyhet.png')}
-          />
+          <View style={styles.css_noodles}>
+                        <View>
+                            <Image style={styles.image_noodles} source={require('../../images/lymohinh.png')} />
+                            </View>
+                            <View>    
+                            <Image style={styles.image_noodles2} source={require('../../images/lymohinh.png')} />
+                            </View>
+                            <View>
+                            <Image style={styles.image_noodles} source={require('../../images/lymohinh.png')} />
+                            </View>
+                    </View>
         </View>
       </ImageBackground>
     </SafeAreaView>
-  );
-};
+      );
+    };
+                         
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+      },
+      logo: {
+        width: 150,
+        height: 120,
+        alignSelf: 'center',
+        marginTop: 10,
+      },
+      text_Outof: {
+        alignSelf: 'center',
+        fontSize: 30,
+        marginTop: 10,
+        color: '#C71A1A',
+        fontFamily: 'SVN-Nexa Rust Slab Black Shadow',
+      },
+      text_content: {
+        color: '#C71A1A',
+        fontSize: 20,
+        fontWeight: 'bold',
+      },
+      css_noodles: {
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        marginHorizontal: 50,
+        marginTop: 20,
+    },
+      image_noodles: {
+        width: 110,
+        height: 190,
+        alignSelf: 'center',
+        marginTop: 30,
+      },
+      image_noodles2: {
+        width: 125,
+        height: 215,
+        alignSelf: 'center',
+        marginTop: 30,
+      },
+    });
+    
+    export default Outof;
+    
+                         
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  logo: {
-    width: 150,
-    height: 120,
-    alignSelf: 'center',
-    marginTop: 10,
-  },
-  text_Outof: {
-    alignSelf: 'center',
-    fontSize: 30,
-    marginTop: 10,
-    color: '#C71A1A',
-    fontFamily: 'SVN-Nexa Rust Slab Black Shadow',
-  },
-  text_content: {
-    color: '#C71A1A',
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  image_noodles: {
-    width: 120,
-    height: 200,
-    alignSelf: 'center',
-    marginTop: 30,
-  },
-});
+                        
 
-export default Outof;
+                        
+                            
+
+                           
+  
