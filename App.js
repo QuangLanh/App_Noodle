b06bbac312bdc73c596a0496e88af89b88f38b35
QuangLanh <tpsf.q.lanh@gmail.com>
2021-12-30 08:56:31 +0700

    update app.js

--- App.js
+++ App.js
@@ -14,7 +14,9 @@ export default function App() {
   return (
 
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator 
+          screenOptions={{
+          headerShown: false}}>
         <Stack.Screen component={Welcome} name='Welcome' />
         <Stack.Screen component={Info} name='Info' />
         <Stack.Screen component={Done} name='Done' />
