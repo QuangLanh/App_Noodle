401e2b148f33e110abee8ed131f7be8c97b48522
QuangLanh <tpsf.q.lanh@gmail.com>
2022-03-28 19:04:54 +0700

    update timeout+welcome

--- App.js
+++ App.js
@@ -3,13 +3,15 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import Welcome from "./src/screens/Welcome";
+
 import Login from "./src/screens/Login";
 import Register from "./src/screens/Register";
 import Categories from "./src/screens/Categories";
 import Payment from "./src/screens/Payment";
 import Credit from "./src/screens/Credit";
 import add_Card from "./src/screens/add_Card";
-import save_Card from "./src/screens/save_Card";
+import save_Card from "./src/screens/save_Card.js";
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
@@ -19,6 +21,7 @@ export default function App() {
           headerShown: false,
         }}
       >
+        <Stack.Screen component={Welcome} name="Welcome" />
         <Stack.Screen component={Login} name="Login" />
         <Stack.Screen component={Register} name="Register" />
         <Stack.Screen component={Categories} name="Categories" />
